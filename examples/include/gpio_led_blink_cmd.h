34466ad9fb9f6aa42751ffb1d56f6561aa7f4d1f
Michael Bernhardt <michael.bernhardt@tq-group.com>
2022-05-12 10:04:08 +0200

    cleanup gpio LED command
    Signed-off-by: Michael Bernhardt <michael.bernhardt@tq-group.com>

--- examples/include/gpio_led_blink_cmd.h
+++ examples/include/gpio_led_blink_cmd.h
@@ -1,19 +1,59 @@
-/*
- * gpio_led_blink_cmd.h
+/** SPDX-License-Identifier: BSD-3-Clause
  *
- *  Created on: 14.04.2022
- *      Author: schneiderj
+ * @file gpio_led_blink_cmd.h
+ * @copyright Copyright (c) 2022 TQ-Systems GmbH <license@tq-group.com>, D-82229 Seefeld, Germany.
+ * @author Jeremias Schneider
+ * @author Michael Bernhardt
+ *
+ * @date 2022-04-14
+ *
+ * -----------------------------------------------------------------------------
+ * @brief This file contains the declaration of the gpio LEDs.
+ *
  */
 
 #ifndef GPIO_LED_BLINK_CMD_H_
 #define GPIO_LED_BLINK_CMD_H_
 
+/*******************************************************************************
+ * includes
+ ******************************************************************************/
+
 #include "portmacro.h"
 #include "projdefs.h"
 #include "FreeRTOS_CLI.h"
 
+/*******************************************************************************
+ * defines
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * macros
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * typedefs
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * prototypes
+ ******************************************************************************/
+
+void gpio_led_blink_main(void *args);
+BaseType_t ledBlinkCommand(char *pcWriteBuffer, __size_t xWriteBufferLen, const char *pcCommandString);
+
+/*******************************************************************************
+ * global extern data
+ ******************************************************************************/
+
 extern const CLI_Command_Definition_t ledBlinkCommandDef;
 
-BaseType_t ledBlinkCommand(char *pcWriteBuffer, __size_t xWriteBufferLen, const char *pcCommandString);
+/******************************************************************************/
 
 #endif /* GPIO_LED_BLINK_CMD_H_ */
