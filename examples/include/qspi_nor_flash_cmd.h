966c1e8cc3daaacb0da02d658191f5e68b4e9cac
Michael Bernhardt <michael.bernhardt@tq-group.com>
2022-05-23 11:24:55 +0200

    add QSPI folder
    Signed-off-by: Michael Bernhardt <michael.bernhardt@tq-group.com>

---
+++ examples/include/qspi_nor_flash_cmd.h
@@ -0,0 +1,65 @@
+/**
+ * @file qspi_nor_flash_cmd.h
+ * @author Copyright (c) 2022, TQ-Systems GmbH
+ * @author TODO author of the file <TODO@tq-group.com>
+ * @date 2022-05-23
+ *
+ * This software code contained herein is licensed under the terms and
+ * conditions of the TQ-Systems Software License Agreement Version 1.0.2.
+ * You may obtain a copy of the TQ-Systems Software License Agreement in the
+ * folder TQS (TQ-Systems Software Licenses) at the following website:
+ * https://www.tq-group.com/Software-Lizenzbedingungen
+ * In case of any license issues please contact license@tq-group.com.
+ *
+ * -----------------------------------------------------------------------------
+ * @brief <TODO short description of the file (only one line)>
+ *
+ * <TODO Detailed description of the file>
+ */
+
+#ifndef EXAMPLES_INCLUDE_QSPI_NOR_FLASH_CMD_H_
+#define EXAMPLES_INCLUDE_QSPI_NOR_FLASH_CMD_H_
+
+/*******************************************************************************
+ * includes
+ ******************************************************************************/
+
+/* runtime */
+
+/* project */
+
+/*******************************************************************************
+ * defines
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * macros
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * typedefs
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * prototypes
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * global extern data
+ ******************************************************************************/
+
+
+
+/******************************************************************************/
+
+#endif /* EXAMPLES_INCLUDE_QSPI_NOR_FLASH_CMD_H_ */
+
+/*[EOF]************************************************************************/
