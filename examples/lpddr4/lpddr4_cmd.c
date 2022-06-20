d9b99c7cb4ae402e3d7b9ea958c05f16280132d0
Michael Bernhardt <michael.bernhardt@tq-group.com>
2022-06-20 10:40:00 +0200

    init lpddr4 via cli  (wip)
    dead loop in init function
    
    Signed-off-by: Michael Bernhardt <michael.bernhardt@tq-group.com>

--- examples/lpddr4/lpddr4_cmd.c
+++ examples/lpddr4/lpddr4_cmd.c
@@ -20,7 +20,8 @@
 /* project */
 #include "projdefs.h"
 #include <drivers/ddr.h>
-//#include "drivers/ddr/v0/soc/am64x_am243x/board_ddrReginit.h"
+//#include "drivers/ddr/v0/soc/am64x_am243x/board_lpddrReginit.h"
+#include "drivers/board_ddrReginit.h"
 /* own */
 #include "lpddr4_cmd.h"
 
@@ -70,24 +71,24 @@ static bool writeLpddr4(void)
     bool    success = false;
     int32_t status  = 0;
 
-//    static DDR_Params gDdrParams =
-//    {
-//        /* below values are set using the globals defined in drivers/ddr/v0/soc/am64x_am243x/board_ddrReginit.h */
-//        .clk1Freq              = DDRSS_PLL_FREQUENCY_1,
-//        .clk2Freq              = DDRSS_PLL_FREQUENCY_2,
-//        .ddrssCtlReg           = DDRSS_ctlReg,
-//        .ddrssPhyIndepReg      = DDRSS_phyIndepReg,
-//        .ddrssPhyReg           = DDRSS_phyReg,
-//        .ddrssCtlRegNum        = DDRSS_ctlRegNum,
-//        .ddrssPhyIndepRegNum   = DDRSS_phyIndepRegNum,
-//        .ddrssPhyRegNum        = DDRSS_phyRegNum,
-//        .ddrssCtlRegCount      = DDRSS_CTL_REG_INIT_COUNT,
-//        .ddrssPhyIndepRegCount = DDRSS_PHY_INDEP_REG_INIT_COUNT,
-//        .ddrssPhyRegCount      = DDRSS_PHY_REG_INIT_COUNT,
-//        .fshcount              = DDRSS_PLL_FHS_CNT,
-//    };
-//
-//    status = DDR_init(&gDdrParams);
+    static DDR_Params gDdrParams =
+    {
+        /* below values are set using the globals defined in drivers/ddr/v0/soc/am64x_am243x/board_ddrReginit.h */
+        .clk1Freq              = DDRSS_PLL_FREQUENCY_1,
+        .clk2Freq              = DDRSS_PLL_FREQUENCY_2,
+        .ddrssCtlReg           = DDRSS_ctlReg,
+        .ddrssPhyIndepReg      = DDRSS_phyIndepReg,
+        .ddrssPhyReg           = DDRSS_phyReg,
+        .ddrssCtlRegNum        = DDRSS_ctlRegNum,
+        .ddrssPhyIndepRegNum   = DDRSS_phyIndepRegNum,
+        .ddrssPhyRegNum        = DDRSS_phyRegNum,
+        .ddrssCtlRegCount      = DDRSS_CTL_REG_INIT_COUNT,
+        .ddrssPhyIndepRegCount = DDRSS_PHY_INDEP_REG_INIT_COUNT,
+        .ddrssPhyRegCount      = DDRSS_PHY_REG_INIT_COUNT,
+        .fshcount              = DDRSS_PLL_FHS_CNT,
+    };
+
+    status = DDR_init(&gDdrParams);
 
     if (status == SystemP_SUCCESS)
     {
