a7852be7952e68ac1d2fab686bd95bb8c6810f0d
Michael Bernhardt <michael.bernhardt@tq-group.com>
2023-02-17 18:35:38 +0100

    add cli afe help function
    Signed-off-by: Michael Bernhardt <michael.bernhardt@tq-group.com>

--- examples/afe/src/afe_cmd.c
+++ examples/afe/src/afe_cmd.c
@@ -16,7 +16,7 @@
  ******************************************************************************/
 
 /* runtime */
-
+#include "string.h"
 /* project */
 #include "afe.h"
 /* own */
@@ -36,7 +36,18 @@
 const CLI_Command_Definition_t afeCommandDef =
 {
     "afe",
-    "\r\nafe [afe]:\r\n AFE access.\r\n\r\n",
+    "\r\nafe [afe]:\r\n AFE access.\n"
+    " Usage afe [access] [channel] [value]:\n"
+    " access:\n"
+    "  adc  - read adc\n"
+    "  temp - read DIE temperature\n"
+    "  gpio - read GPIO state\n"
+    "  set  - set GPIO state\n"
+    " channel:\n"
+    "  0 - 11 ADC channel\n"
+    "  0 - 9 GPIO channel\n"
+    " state:\n"
+    "  0, 1\r\n\r\n",
     afeCommand,
     -1
 };
@@ -73,11 +84,10 @@ const CLI_Command_Definition_t afeCommandDef =
 BaseType_t afeCommand( char *pcWriteBuffer, __size_t xWriteBufferLen, const char *pcCommandString )
 {
     uint32_t counter                  = 0;
-    uint8_t  i                        = 0;
-    float val = 0;
-    uint8_t gpio = 0;
-    char*      pcParameter[3] = {NULL};
-    BaseType_t xParameterStringLength  = 0;
+    uint8_t gpio                      = 0;
+    float val                         = 0;
+    char* pcParameter[3]              = {NULL};
+    BaseType_t xParameterStringLength = 0;
 
     /* get all parameter */
     for (counter = 0; counter < 3; counter++)
@@ -85,25 +95,35 @@ BaseType_t afeCommand( char *pcWriteBuffer, __size_t xWriteBufferLen, const char
         pcParameter[counter] = (char*) FreeRTOS_CLIGetParameter(pcCommandString, counter + 1, &xParameterStringLength);
     }
 
-    if (!strcmp(pcParameter[0], "adc"))
+    if (!strncmp(pcParameter[0], "adc", 3))
     {
-        val = AFE_ReadAdcVoltage(*pcParameter[1] -'0');
-        sprintf(&pcWriteBuffer[0], "Voltage %.1fV\r\n\n", val);
+        if (pcParameter[1] != NULL)
+        {
+            val = AFE_ReadAdcVoltage(*pcParameter[1] - '0');
+            sprintf(&pcWriteBuffer[0], "Voltage %.1fV\r\n\n", val);
+        }
     }
-    else if(!strcmp(pcParameter[0], "temp"))
+    else if (!strcmp(pcParameter[0], "temp"))
     {
         val = AFE_ReadDieTemp();
         sprintf(&pcWriteBuffer[0], "Temperature %.1f%cC\r\n\n", val, (uint8_t)176);
     }
-    else if(!strcmp(pcParameter[0], "gpio"))
+    else if (!strncmp(pcParameter[0], "gpio", 4))
     {
-        AFE_GpioSet(*pcParameter[1] -'0', *pcParameter[2] -'0');
-        sprintf(&pcWriteBuffer[0], "OK \r\n\n");
+        if (pcParameter[1] != NULL)
+        {
+            gpio = AFE_GpioRead(*pcParameter[1] - '0');
+            sprintf(&pcWriteBuffer[0], "GPIO %d \r\n\n", gpio);
+        }
+
     }
-    else if(!strcmp(pcParameter[0], "set"))
+    else if (!strncmp(pcParameter[0], "set", 3))
     {
-        gpio = AFE_GpioRead(*pcParameter[1] -'0');
-        sprintf(&pcWriteBuffer[0], "GPIO %d \r\n\n", gpio);
+        if ((pcParameter[1] != NULL) && (pcParameter[2] != NULL))
+        {
+            AFE_GpioSet(*pcParameter[1] - '0', *pcParameter[2] - '0');
+            sprintf(&pcWriteBuffer[0], "OK \r\n\n");
+        }
     }
 
     return pdFALSE;
